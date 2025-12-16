f9c2c154cb0990f6a27a5269447bf1ae4366774d
Ismael <ismaelmunozgranados@gmail.com>
2025-12-16 17:58:40 +0100

    added gzip - brotli

--- Modulo_3-Bundling/01_Laboratorio/vite/vite.config.ts
+++ Modulo_3-Bundling/01_Laboratorio/vite/vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from "vite";
 import checker from "vite-plugin-checker";
 import { analyzer } from "vite-bundle-analyzer";
+import { compression } from "vite-plugin-compression2";
 
 export default defineConfig({
-    plugins : [checker({typescript: true}), analyzer()],
+    plugins : [
+        checker({typescript: true}), 
+        analyzer(), 
+        compression({
+            threshold : 1024,
+            algorithms : [
+                'gzip',
+                'brotliCompress'
+            ]
+        })
+    ],
 });
