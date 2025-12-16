cc3ef1b7c22e78d91cdf353bf6987f0836d6f9d7
Ismael <ismaelmunozgranados@gmail.com>
2025-12-16 13:11:33 +0100

    Lab Vite terminado

--- Modulo_3-Bundling/01_Laboratorio/vite/src/index.ts
+++ Modulo_3-Bundling/01_Laboratorio/vite/src/index.ts
@@ -6,10 +6,6 @@ title.innerHTML = "Esto es un H1 estilado con CSS Modules.";
 title.classList.add(classes.title);
 document.body.appendChild(title);
 
-const text_line1 = document.createElement("p");
-text_line1.innerHTML = `Api server is ${config.API_BASE}`;
-document.body.appendChild(text_line1);
-
-const text_line2 = document.createElement("p");
-text_line2.innerHTML = `Feature A is ${config.IS_FEATURE_A_ENABLED ? "enabled" : "disabled"}`;
-document.body.appendChild(text_line2);
+const text_env = document.createElement("p");
+text_env.innerHTML = `Esta es una variable de entorno en ${config.TEXT}`;
+document.body.appendChild(text_env);
--- Modulo_3-Bundling/01_Laboratorio/vite/src/vite-env.d.ts
+++ Modulo_3-Bundling/01_Laboratorio/vite/src/vite-env.d.ts
@@ -1,6 +1,5 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-    readonly VITE_API_BASE : string
-    readonly VITE_ENABLE_FEATURE_A: string
+    readonly VITE_TEXT : string
 }
